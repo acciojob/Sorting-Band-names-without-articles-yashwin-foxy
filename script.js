9c4671d53cb68e8956784821dfeb8a0b06292dc5
yashwin-foxy <139614461+yashwin-foxy@users.noreply.github.com>
2023-09-20 14:31:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
 
+ function removeArticles(name) {
+            return name.replace(/^(a |an |the )/i, '').trim();
+        }
+
+        // Array of band names
+        let bandNames = ['The Rolling Stones', 'The Beatles', 'Led Zeppelin', 'Pink Floyd', 'Aerosmith'];
+
+        // Remove articles and sort the array
+        bandNames.sort((a, b) => {
+            const nameA = removeArticles(a).toLowerCase();
+            const nameB = removeArticles(b).toLowerCase();
+            return nameA.localeCompare(nameB);
+        });
+
+        // Get the ul element
+        const ul = document.getElementById('band');
+
+        // Add band names to the ul element
+        bandNames.forEach(name => {
+            const li = document.createElement('li');
+            li.textContent = name;
+            ul.appendChild(li);
+        });
+    
+
+
+
+
+
